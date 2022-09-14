8f3d0ca461c09abdbd18efad04d5181242b6ee51
gautam1697 <gautam.atre16@gmail.com>
2022-09-14 13:15:23 +0530

    68 files deleted

--- local_global.js
+++
@@ -1,9 +0,0 @@
-a=10
-function func(){
-    console.log(a);
-    console.log(this.a)
-}
-func()
-console.log(a)
-// console.log(this.a)
-//how to access global varibale inside a local scope
--- odessa.js
+++
@@ -1,4 +0,0 @@
-console.log('odessa file')
-console.log('this is a modified file again')
-console.log('this file s also changed but us tracked and the status will beome modified');
-console.log('this is a new change and going t third sjdjsdncommand -m');
