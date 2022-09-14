659970c48409791a56eb3de5980f83457014e7c0
gautam1697 <gautam.atre16@gmail.com>
2022-09-14 12:50:19 +0530

    removed new files

---
+++ newfile.js
@@ -0,0 +1,4 @@
+console.log('we are making change sin this file and going to commit it');
+console.log('aduiassdasdioadnanddbada');
+console.log('fbskfnskfsdhsb');
+console.log('new console line to check if git reset is workin');
